db9307774dd06248628b3e7653baa4e67b2277b7
Leonid <leonid.p@akitorg.ru>
2024-02-19 16:45:53 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='evm_wallet',
+   version='0.1.0',
+   description='Module to work with EVM web3 wallets',
+   author='GuessWho',
+   author_email='eu1qjqaw8dda@mail.ru',
+   packages=['evm_wallet'],
+   install_requires=['wheel', 'bar', 'greek']
+)
